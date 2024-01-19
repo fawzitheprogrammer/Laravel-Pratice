255e9e132639cdf524c92f8da7bd24626ae1ad6a
Fawzi Gharib <fawz11gharib@gmail.com>
2024-01-19 22:40:22 +0300

    Table Added

--- database/migrations/2024_01_05_134254_add_new_column_to_user_table.php
+++ database/migrations/2024_01_05_134254_add_new_column_to_user_table.php
@@ -1,4 +1,4 @@
-<?php
+/***<?php
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
@@ -6,23 +6,23 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::table('users_with_pro', function (Blueprint $table) {
-            $table->string('profile_pic');
-        });
-    }
+    // /**
+    //  * Run the migrations.
+    //  */
+    // public function up(): void
+    // {
+    //     Schema::table('users_with_pro', function (Blueprint $table) {
+    //         $table->string('profile_pic');
+    //     });
+    // }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::table('user_and_pro', function (Blueprint $table) {
-            //
-        });
-    }
+    // /**
+    //  * Reverse the migrations.
+    //  */
+    // public function down(): void
+    // {
+    //     Schema::table('user_and_pro', function (Blueprint $table) {
+    //         //
+    //     });
+    // }
 };
