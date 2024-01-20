da03ce1e00b0fecc38d0c6c4f86e46d9d5fb8c3c
Fawzi Gharib <fawz11gharib@gmail.com>
2024-01-20 23:28:45 +0300

    Some Styling

--- resources/views/register-user/registeration.blade.php
+++ resources/views/register-user/registeration.blade.php
@@ -29,9 +29,9 @@
       <div>
         <h3 class="mb-4 font-semibold text-gray-900 dark:text-white">Technology</h3>
 <ul
-    class="w-48 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+    class="w-48 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white pl-3">
     <li class="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
-        <div class="flex items-center ps-3">
+        <div class="flex items-center ps-3 ">
             <input id="vue-checkbox" type="checkbox" value="Vue" name="skills[]"
                 class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
             <label for="vue-checkbox" class="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Vue
@@ -147,7 +147,7 @@
 
 
       </div>
-      <div class="flex items-center justify-between">
+      <div class="flex items-center justify-between pt-3">
         <button  class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
           Add
         </button>
